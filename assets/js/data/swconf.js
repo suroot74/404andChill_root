18fb1777cf5eb4bb916151928513617f6e3e9a35
suroot74 <suroot74@users.noreply.github.com>
2025-01-12 13:41:31 +0000

    deploy: 1b913eeab8d1849cb6022a3d259c1312a297c14b

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -431,7 +431,7 @@
 <!-- return -->
 const swconf = {
   
-    cacheName: 'chirpy-1736689144',resources: [
+    cacheName: 'chirpy-1736689285',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
@@ -449,8 +449,6 @@ const swconf = {
     interceptor: {paths: [
         
       ],urlPrefixes: [
-        
-          'https://404andchill.goatcounter.com/counter/'
         
       ]
     },
