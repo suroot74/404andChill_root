c686750d1c2ce002482fcd923f353a91b9edbeab
suroot74 <suroot74@users.noreply.github.com>
2025-01-14 01:13:43 +0000

    deploy: 4944996d18a57507300cd565d39ca5c5aba9c1a6

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -426,7 +426,7 @@
 <!-- return -->
 const swconf = {
   
-    cacheName: 'chirpy-1736804250',resources: [
+    cacheName: 'chirpy-1736817211',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
